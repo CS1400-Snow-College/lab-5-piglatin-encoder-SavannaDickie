e0f8c44022735768e51dec9286f727631d9571c0
Savanna Dickie <personal3@Savannas-MacBook-Pro.local>
2024-10-15 19:41:52 -0600

    Collect a string

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 // Savanna Dickie
 // 10/15/2024
 // Lab 5 piglatin encoder
 
 Console.WriteLine("Welcome to the PigLatin Cryptogram\nThis program is going to translate your message into pig Latin,\nthen it will encrypt the message into a Cryptogram.");
+Console.WriteLine("Please enter the message: ");
+string? userMessage = Console.ReadLine();
+
+string[] userMessageArray = userMessage.Split(' ');
+//Console.WriteLine(userMessageArray);
+foreach (string word in userMessageArray)
+{
+    Console.WriteLine(word);
+}
+
+
+//char[] vowel = {'a','e','i','o','u'};
+//char[]
